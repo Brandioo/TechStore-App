e23a36cd0b84a2b5e84c38f44274421b23488baa
Brand Citozi <citozibrand@gmail.com>
2021-02-10 17:53:46 +0100

    Validations Of Web Application

--- src/main/java/AbstractFactory/StatusCategory.java
+++ src/main/java/AbstractFactory/StatusCategory.java
@@ -6,8 +6,8 @@ import model.Employee;
 import java.util.Optional;
 
 public interface StatusCategory {
-    void start(Stage primaryStage) throws Exception;
+    void start(Stage primaryStage) throws ExceptionExample;
 
-    Optional<Employee> getAction() throws Exception;
+    Optional<Employee> getAction() throws ExceptionExample;
 
 }
